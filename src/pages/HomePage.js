5c980634e05d4e35c34908df232565f783f7300e
Harsh <harshkhanna208@gmail.com>
2024-07-02 23:25:50 +0530

    Homepage update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const HomePage = () => {
-  return <Layout title="HomePage">HomePage</Layout>;
+  const [auth, setAuth] = useAuth();
+  return (
+    <Layout title="HomePage">
+      HomePage
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    </Layout>
+  );
 };
 
 export default HomePage;
