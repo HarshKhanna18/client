317be10b83b2367fdc4e487c6671d0a3d20b0032
Harsh <harshkhanna208@gmail.com>
2024-07-05 09:18:18 +0530

    Private route file updattion

--- src/components/Routes/privateRoutes.js
+++ src/components/Routes/privateRoutes.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
+import { Outlet } from "react-router-dom";
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth(); //destructure the auth
 
   useEffect(() => {}, []);
-  return ok? 'spinner'
+  return ok ? <Outlet /> : "spinner";
 }
