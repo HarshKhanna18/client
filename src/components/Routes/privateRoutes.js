a38fcce43d395d9e2b72bae0a507cfbf6d069a99
Harsh <harshkhanna208@gmail.com>
2024-07-05 09:31:52 +0530

    Private route updattion

--- src/components/Routes/privateRoutes.js
+++ src/components/Routes/privateRoutes.js
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
+import axios from "axios";
 
 export default function PrivateRoute() {
   const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth(); //destructure the auth
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const authCheck = async () => {
+      //defining prefix of our private URL
+      const res = await axios.get("/api/v1/auth/user-auth", {
+        headers: {
+          Authorization: auth?.token, //defining header with key<token>
+        },
+      });
+      if (res.data.ok) {
+        setOk(true);
+      } else {
+        setOk(false);
+      }
+    };
+    if (auth.token) authCheck(); //caling hook function
+  }, [auth?.token]); //defining depemndency
   return ok ? <Outlet /> : "spinner";
 }
